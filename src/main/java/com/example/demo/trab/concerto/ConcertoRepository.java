1ab83af2b6485cfb7d936b52e4b5d0b44257d007
giullix <giullia.coloridah@gmail.com>
2025-04-23 09:03:29 -0300

    parte 1 - trab 3

--- src/main/java/com/example/demo/trab/concerto/ConcertoRepository.java
+++ src/main/java/com/example/demo/trab/concerto/ConcertoRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.trab.concerto;
 
-import com.example.demo.medico.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConcertoRepository extends JpaRepository<Concerto, Long>{
